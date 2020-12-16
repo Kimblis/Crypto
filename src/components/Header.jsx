e4df50f762b195fd18834eb13f618bef088dc5d4
unknown <E.Masilionis122@gmail.com>
2020-12-16 15:09:38 +0200

    Done

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-center text-warning mt-5 mb-4">Cryptos</h1>
+    </div>
+  );
+};
+
+export default Header;
